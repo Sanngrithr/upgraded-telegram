2c2234f9573ea01796161e2b9ac4700e2488e917
Sanngrithr <tomislav.sever@gmx.de>
2018-10-17 10:35:31 +0200

    Created cs console program L01

---
+++ L01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace L01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(args[0]);
+        }
+    }
+}
